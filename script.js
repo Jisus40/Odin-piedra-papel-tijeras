f157a8ab9deb6bfbd5cfeb52d612ae221a0a1c87
JISUS <miratrizjesus1@gmail.com>
2025-07-22 18:46:44 -0430

    excersises solution

---
+++ script.js
@@ -0,0 +1,70 @@
+function getHumanChoice() {
+    let human = prompt("select: rock, paper or scissors");
+    return human.toLocaleLowerCase();
+}
+
+
+function getComputerChoice() {
+    let computer = Math.random() * 100 + 51;
+    if (computer <= 50) {
+        return "rock";
+    }else if (computer <= 100) {
+        return "paper";
+    }else {
+        return "scissors";
+    }
+};
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    //const humanSelection = getHumanChoice();
+    //const computerSelection = getComputerChoice();
+    function palyRound(humanChoice, computerChoice) {
+    if (humanChoice == "paper") {
+        if (computerChoice == "scissors") {
+            computerScore++;
+            console.log("perdiste, punto para la computadora");
+        }else if (computerChoice == "rock") {
+            humanScore++;
+            console.log("ganaste, punto para ti");
+        }else {
+            console.log("empate");
+        }
+    }else if (humanChoice == "rock") {
+        if (computerChoice == "paper") {
+            computerScore++;
+            console.log("perdiste, punto para la computadora");
+        }else if (computerChoice == "scissors") {
+            humanScore++;
+            console.log("ganaste, punto para ti");
+        }else {
+            console.log("empate");
+        }
+    }else if (humanChoice == "scissors") {
+        if (computerChoice == "rock") {
+            computerScore++;
+            console.log("perdiste, punto para la computadora");
+        }else if (computerChoice == "paper") {
+            humanScore++;
+            console.log("ganaste, punto para ti");
+        }else {
+            console.log("empate");
+        }
+    }else {
+        console.log("error, no introduciste un valor valido");
+    }
+}
+    palyRound(getHumanChoice(), getComputerChoice());
+    console.log("tu: " + humanScore + " " + "la computadora: " + computerScore);
+    palyRound(getHumanChoice(), getComputerChoice());
+    console.log("tu: " + humanScore + " " + "la computadora: " + computerScore);
+    palyRound(getHumanChoice(), getComputerChoice());
+    console.log("tu: " + humanScore + " " + "la computadora: " + computerScore);
+    palyRound(getHumanChoice(), getComputerChoice());
+    console.log("tu: " + humanScore + " " + "la computadora: " + computerScore);
+    palyRound(getHumanChoice(), getComputerChoice());
+    console.log("tu: " + humanScore + " " + "la computadora: " + computerScore);
+    console.log("final: tu: " + humanScore + " la computadora: " + computerScore);
+}
+playGame();
