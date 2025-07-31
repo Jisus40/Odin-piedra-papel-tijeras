1b157fb87c3ebc308525f17af112b3ca0d18510b
JISUS <miratrizjesus1@gmail.com>
2025-07-30 21:48:35 -0430

    refactoring

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-function getHumanChoice() {
-    let human = prompt("select: rock, paper or scissors");
-    return human.toLocaleLowerCase();
-}
-
-
 function getComputerChoice() {
     let computer = Math.floor(Math.random() * 3);
     if (computer == 0) {
@@ -15,56 +9,53 @@ function getComputerChoice() {
     }
 };
 
-function playGame() {
     let humanScore = 0;
     let computerScore = 0;
-    //const humanSelection = getHumanChoice();
-    //const computerSelection = getComputerChoice();
-    function palyRound(humanChoice, computerChoice) {
-    if (humanChoice == "paper") {
-        if (computerChoice == "scissors") {
-            computerScore++;
-            console.log("perdiste, punto para la computadora");
-        }else if (computerChoice == "rock") {
-            humanScore++;
-            console.log("ganaste, punto para ti");
-        }else {
-            console.log("empate");
-        }
-    }else if (humanChoice == "rock") {
-        if (computerChoice == "paper") {
-            computerScore++;
-            console.log("perdiste, punto para la computadora");
-        }else if (computerChoice == "scissors") {
-            humanScore++;
-            console.log("ganaste, punto para ti");
-        }else {
-            console.log("empate");
-        }
-    }else if (humanChoice == "scissors") {
-        if (computerChoice == "rock") {
-            computerScore++;
-            console.log("perdiste, punto para la computadora");
-        }else if (computerChoice == "paper") {
-            humanScore++;
-            console.log("ganaste, punto para ti");
-        }else {
-            console.log("empate");
-        }
+
+    function playRound(humanChoice, computerChoice) {
+        const paragrahp = document.createElement("p");
+        
+    if (humanChoice == "paper" && computerChoice == "rock") {
+        paragrahp.textContent = "ganaste";
+        humanScore++;
+    }else if (humanChoice == "rock" && computerChoice == "scissors") {
+        paragrahp.textContent = "ganaste";
+        humanScore++;
+    }else if (humanChoice == "scissors" && computerChoice == "paper") {
+        paragrahp.textContent = "ganaste";
+        humanScore++;
+    }else if (humanChoice == computerChoice) {
+        paragrahp.textContent = "empate";
     }else {
-        console.log("error, no introduciste un valor valido");
+        paragrahp.textContent = "perdiste";
+        computerScore++;
     }
-}
-    palyRound(getHumanChoice(), getComputerChoice());
-    console.log("tu: " + humanScore + " " + "la computadora: " + computerScore);
-    palyRound(getHumanChoice(), getComputerChoice());
-    console.log("tu: " + humanScore + " " + "la computadora: " + computerScore);
-    palyRound(getHumanChoice(), getComputerChoice());
-    console.log("tu: " + humanScore + " " + "la computadora: " + computerScore);
-    palyRound(getHumanChoice(), getComputerChoice());
-    console.log("tu: " + humanScore + " " + "la computadora: " + computerScore);
-    palyRound(getHumanChoice(), getComputerChoice());
-    console.log("tu: " + humanScore + " " + "la computadora: " + computerScore);
-    console.log("final: tu: " + humanScore + " la computadora: " + computerScore);
-}
-playGame();
+    
+     div.appendChild(paragrahp);    
+};
+
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissors = document.querySelector(".scissors");
+const div = document.querySelector(".div");
+const result = document.createElement("p");
+
+rock.addEventListener("click", () => playerSelection("rock"));
+
+paper.addEventListener("click", () => playerSelection("paper"));
+
+scissors.addEventListener("click", () => playerSelection("scissors"));
+
+function playerSelection(selected) {
+    playRound(selected, getComputerChoice());
+    if (humanScore < 5 && computerScore < 5) {
+        result.textContent = `tu: ${humanScore}, la computadora: ${computerScore}`;
+        div.appendChild(result);
+    } else if (humanScore >= 5 && computerScore < 5) {
+        result.textContent = `tu ganas: ${humanScore} a ${computerScore}`;
+    } else if (humanScore < 5 && computerScore >= 5) {
+        result.textContent = `tu pierdes: ${humanScore} a ${computerScore}`;
+    } else {
+        result.textContent = `empate: ${humanScore} a ${computerScore}`;
+    };
+};
